4dda93a1e17f90b02468552823ea34c9f4951d36
krzysiek-smaga <krzysiek.at@gmail.com>
2023-04-07 16:33:27 +0200

    Implement and test GetEmployees endpoint to get all empleyees

--- EmployeeApp.API/Controllers/EmployeesController.cs
+++ EmployeeApp.API/Controllers/EmployeesController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeApp.API.Context;
+using EmployeeApp.API.Domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeApp.API.Controllers
 {
@@ -7,5 +10,17 @@ namespace EmployeeApp.API.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly EmployeeApiDbContext _context;
+
+        public EmployeesController(EmployeeApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
     }
 }
