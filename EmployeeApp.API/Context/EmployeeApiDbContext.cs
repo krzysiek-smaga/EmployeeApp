5c3200a751b8f5506f60135c647ccd249ae8823e
krzysiek-smaga <krzysiek.at@gmail.com>
2023-04-07 16:13:01 +0200

    Implement EmployeeApiDbContext with Initial migration

---
+++ EmployeeApp.API/Context/EmployeeApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeApp.API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.API.Context
+{
+    public class EmployeeApiDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeApiDbContext(DbContextOptions<EmployeeApiDbContext> options) : base(options) { }
+    }
+}
