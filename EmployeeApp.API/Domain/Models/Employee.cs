027f1e15ddd11999750cf7ca9a8f4e6145ba1d75
krzysiek-smaga <krzysiek.at@gmail.com>
2023-04-07 15:57:33 +0200

    Add Employee entity

---
+++ EmployeeApp.API/Domain/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeApp.API.Domain.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+        public int? ManagerId { get; set; }
+    }
+}
